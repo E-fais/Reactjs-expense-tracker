6ca8990536db9ecffc6b5f23d88fb36fda15db31
fais edathil <faiesedathil@gmail.com>
2022-08-30 23:37:16 +0300

    Updates

---
+++ precache-manifest.5ce007fc6c67c1602d0994700ac13fda.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e70c3e72d3df0f61eec0",
+    "url": "/Reactjs-expense-tracker/static/js/main.e70c3e72.chunk.js"
+  },
+  {
+    "revision": "ed7078fe13c00db95b0e",
+    "url": "/Reactjs-expense-tracker/static/js/1.ed7078fe.chunk.js"
+  },
+  {
+    "revision": "b733e6e1c959d151547d",
+    "url": "/Reactjs-expense-tracker/static/js/runtime~main.b733e6e1.js"
+  },
+  {
+    "revision": "fc1c19a09a8d599c8699777374230838",
+    "url": "/Reactjs-expense-tracker/index.html"
+  }
+];
